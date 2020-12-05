2faea95554a0f1298c5570799a9b9a2c07c97ec2
Miguel Filipe <miguel.manteigueiro@ubi.pt>
2020-12-05 18:27:36 +0000

    Fixing main

--- src/project/Main.java
+++ src/project/Main.java
@@ -22,7 +22,6 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<Conta> c = new ArrayList<Conta>();
-
         try {
             ObjectInputStream ficheiro = new ObjectInputStream (new FileInputStream("C:\\Users\\Miguel\\Desktop\\UBI\\2o_Ano\\POO\\ProjetoPOO\\src\\project\\contas.dat"));
             c = (ArrayList<Conta>) ficheiro.readObject();
@@ -31,13 +30,16 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("Bem vindo à loja Auto24");
-        //Pessoa p = ;
-        Conta c2 = new Conta(new Pessoa("Quim", "Tenrinho", 333333353, "Rua 69"), "abc123");
-        c2.RegistoConta(c);
-        for (Conta aa: c) {
-            System.out.println(aa.toString());
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        try {
+            ObjectInputStream ficheiro = new ObjectInputStream (new FileInputStream("C:\\Users\\Miguel\\Desktop\\UBI\\2o_Ano\\POO\\ProjetoPOO\\src\\project\\contas.dat"));
+            produtos = (ArrayList<Produto>) ficheiro.readObject();
         }
+        catch (IOException | ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Bem vindo à loja Auto24");
+        
     }
 }
-//teste
